22d81fe81f1e847cc920ca935cf83578a68ac701
savvyinfo <janghkoh@yahoo.com>
2017-10-14 22:09:58 -0700

    deploy nodemailer works

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,80 +8,108 @@ export const HOURS = 'HOURS';
 export const REVIEWS = 'REVIEWS';
 export const SEARCH = 'SEARCH';
 export const SEARCH_RESULT = 'SEARCH_RESULT';
+export const EMAIL = 'EMAIL';
+export const NAME = 'NAME';
+export const SUBMIT = 'SUBMIT';
 
 export const getChefs = () => {
-  const chefs = axios.get('https://nxtdoorchef.com/api/chef')
-    .then(resp => resp.data.data);
+    const chefs = axios.get('https://nxtdoorchef.com/api/chef')
+        .then(resp => resp.data.data);
 
-  return {
-    type: CHEFS,
-    payload: chefs
-  };
+    return {
+        type: CHEFS,
+        payload: chefs
+    };
 };
 
 export const displaySelectedChef = (id) => {
-  const chef = axios.get(`https://nxtdoorchef.com/api/chef/id/${id}`)
-    .then(resp => resp.data.data[0]);
+    const chef = axios.get(`https://nxtdoorchef.com/api/chef/id/${id}`)
+        .then(resp => resp.data.data[0]);
 
-  return {
-    type: CHEF,
-    payload: chef
-  }
+    return {
+        type: CHEF,
+        payload: chef
+    }
 };
 
 export const getMenu = (id) => {
-  const menu = axios.get(`https://nxtdoorchef.com/api/menu/id/${id}`)
-    .then(resp => resp.data.data);
+    const menu = axios.get(`https://nxtdoorchef.com/api/menu/id/${id}`)
+        .then(resp => resp.data.data);
 
-  return {
-    type: MENU,
-    payload: menu
-  };
+    return {
+        type: MENU,
+        payload: menu
+    };
 };
 
 export const getFoodItems = () => {
-  const foods = axios.get(`https://nxtdoorchef.com/api/menu`)
-    .then(resp => resp.data.data[0]);
+    const foods = axios.get(`https://nxtdoorchef.com/api/menu`)
+        .then(resp => resp.data.data[0]);
 
-  return {
-    type: FOOD_LIST,
-    payload: foods
-  };
+    return {
+        type: FOOD_LIST,
+        payload: foods
+    };
 };
 
 export const getHours = (id) => {
-  const hours = axios.get(`https://nxtdoorchef.com/api/hours/chef/${id}`)
-    .then(resp => resp.data.data);
+    const hours = axios.get(`https://nxtdoorchef.com/api/hours/chef/${id}`)
+        .then(resp => resp.data.data);
 
-  return {
-    type: HOURS,
-    payload: hours
-  };
+    return {
+        type: HOURS,
+        payload: hours
+    };
 };
 
 export const getReviews = (id) => {
-  const reviews = axios.get(`https://nxtdoorchef.com/api/reviews/retrieve/${id}`)
-    .then(resp => resp.data.data);
+    const reviews = axios.get(`https://nxtdoorchef.com/api/reviews/retrieve/${id}`)
+        .then(resp => resp.data.data);
 
-  return {
-    type: REVIEWS,
-    payload: reviews
-  };
+    return {
+        type: REVIEWS,
+        payload: reviews
+    };
 };
 
 export const searchInput = (text) => {
-  return {
-    type: SEARCH,
-    payload: text
-  }
+    return {
+        type: SEARCH,
+        payload: text
+    }
 };
 
 export const foodSearch = (food) => {
-  const search = axios.get(`https://nxtdoorchef.com/api/menu/search/${food}`)
-    .then(resp => resp.data.data);
+    const search = axios.get(`https://nxtdoorchef.com/api/menu/search/${food}`)
+        .then(resp => resp.data.data);
 
-  return {
-    type: SEARCH_RESULT,
-    payload: search
-  };
+    return {
+        type: SEARCH_RESULT,
+        payload: search
+    };
+};
+
+export const nameInput = (name) => {
+    return {type: NAME, payload: name};
+};
+
+export const emailInput = (email) => {
+    return {type: EMAIL, payload: email};
+};
+
+export const submit = (email, name, photo, price, meal, address, chef) => {
+    console.log('clicked');
+    const submit = axios.post('https://nxtdoorchef.com/api/email/confirmation', {
+        email: email,
+        user: name,
+        purchase_photo: photo,
+        purchase_price: price,
+        purchase_name: meal,
+        chef_address: address,
+        chef_name: chef
+    })
+        .then(() => 'success')
+        .catch(() => 'error');
+
+    return {type: SUBMIT, payload: submit};
 };
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,187 +1,242 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {displaySelectedChef, getMenu} from '../actions';
+import {displaySelectedChef, getMenu, nameInput, emailInput, submit} from '../actions';
 import Navbar from './functional/Navbar';
 import titleCheckout from '../assets/imgs/section_title_checkout.png';
 import titleCooking from '../assets/imgs/section_title_cooking.png';
 
 class Checkout extends React.Component {
-  state = {ordered: false};
+    state = {ordered: false};
 
-  componentWillMount () {
-    const {displaySelectedChef, getMenu, chefId} = this.props;
+    componentWillMount () {
+        const {displaySelectedChef, getMenu, chefId} = this.props;
 
-    displaySelectedChef(chefId);
-    getMenu(chefId);
-  }
-
-  getItem () {
-    return this.props.menu.find((item) => {
-      return parseInt(this.props.menuId) === item.menu_item_number;
-    });
-  }
-
-  itemOrdered () {
-    if (this.state.ordered) {
-      return (
-          <div className="col-xs-12 col-md-12 col-lg-6">
-          <div className="card" style={styles.buyContainerStyle}>
-            <h4 className="text-center subTitle"><img src={titleCheckout} style={{marginBottom: '10px'}} /><br />CUSTOMER INFO</h4>
-            <form>
-              <div className="form-group">
-                <input type="text" className="form-control burgundyLight-bg inputwords" style={styles.searchInputStyle} placeholder="Enter Name"/>
-                <input type="text" className="form-control burgundyLight-bg inputwords" style={styles.searchInputStyle} placeholder="Enter Email"/>
-              </div>
-              <button className="submit_btn_container" type="submit" style={styles.submitContainerStyle}>
-                <span className="button_title">SUBMIT</span>
-              </button>
-            </form>
-          </div>
-        </div>
-      );
+        displaySelectedChef(chefId);
+        getMenu(chefId);
     }
-  }
 
-  render () {
-    const food = this.getItem();
+    getItem () {
+        return this.props.menu.find((item) => {
+            return parseInt(this.props.menuId) === item.menu_item_number;
+        });
+    }
 
-    return (
-      <div>
-        <Navbar />
-        <div className="row">
-          <div className="col-xs-12 col-md-12 col-lg-6">
-            <div className="card" style={styles.imageContainerStyle}>
+    itemOrdered (food) {
+        // if (this.props.checkout.submitted === 'success') {
+        //   return;
+        // }
+        if (this.state.ordered) {
+            return (
+                <div className="col-xs-12 col-md-12 col-lg-6">
+                    <div className="card" style={styles.buyContainerStyle}>
+                        <h4 className="text-center subTitle"><img src={titleCheckout} style={{marginBottom: '10px'}} /><br />CUSTOMER INFO</h4>
+                        <form>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control burgundyLight-bg inputwords"
+                                    style={styles.searchInputStyle}
+                                    placeholder="Enter Name"
+                                    onChange={(event) => this.props.nameInput(event.target.value)}
+                                    value={this.props.checkout.name}
+                                />
+                                <input
+                                    type="text"
+                                    className="form-control burgundyLight-bg inputwords"
+                                    style={styles.searchInputStyle}
+                                    placeholder="Enter Email"
+                                    onChange={(event) => this.props.emailInput(event.target.value)}
+                                    value={this.props.checkout.email}
+                                />
+                            </div>
+                            <div
+                                className="submit_btn_container"
+                                style={styles.submitContainerStyle}
+                                onClick={() => this.props.submit(this.props.checkout.email, this.props.checkout.name, food.photo, food.price, food.item_name, 'John', this.props.chef.address)}
+                            >
+                                <span className="button_title">SUBMIT</span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            );
+        }
+    }
 
-              <div style={{overflow: 'hidden'}}>
-                <img className="card-img" src={food ? food.photo : ''} alt="picture of selected dish" style={styles.cardImageStyle}/>
-              </div>
-              <div className="card-img-overlay">
-                  <div className="price_btn_container"
-                       style={{position: 'absolute', left: '35%'}}
-                  >
-                    <h6 className="text-center subSubTitle" style={{display: 'block', marginTop: '1.25vmin', marginBottom: '.1vmin'}}>COST:</h6>
-                    <span className="priceNum" style={{display: 'block'}}>${food ? food.price : ''}</span>
-                  </div>
-                  <div onClick={() => this.setState({ordered: !this.state.ordered})}
-                       className="buy_btn_container burgundy-bg"
-                       style={{position: 'absolute', left: '55%'}}
-                  >
-                    <span className="buy_button_title">BUY</span>
-                  </div>
-              </div>
+    goHome () {
+        this.props.history.push("/");
+        location.reload();
+    }
+
+    submitted () {
+        if (this.props.checkout.submitted === ''){
+            return;
+        } else if (this.props.checkout.submitted === 'success') {
+            return (
+                <div className="col-xs-12 col-md-12 col-lg-6">
+                    <div className="card" style={styles.buyContainerStyle}>
+                        <h4 className="text-center subTitle"><img src={titleCheckout} style={{marginBottom: '10px'}} /><br />ORDER CONFIRMATION</h4>
+                        <h4 className="nameTitle text-center">Thank You!</h4>
+                        <p className="mainText text-center">
+                            You have successfully placed your order.<br />
+                            Your nxtDoorChef thanks you for your purchase!<br />
+                            A confirmation has been sent to your email address.
+                        </p>
+                        <div className="submit_btn_container" style={styles.submitContainerStyle} onClick={() => this.goHome()}>
+                            <span className="button_title">CLOSE</span>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else if (this.props.checkout.submitted === 'error'){
+            return (
+                <div className="col-xs-12 col-md-12 col-lg-6">
+                    <div className="card" style={styles.buyContainerStyle}>
+                        <h4 className="text-center subTitle"><img src={titleCheckout} style={{marginBottom: '10px'}} /><br />ORDER ERROR</h4>
+                        <h4 className="nameTitle text-center">Oops!</h4>
+                        <p className="mainText text-center">
+                            Something went wrong. <br />
+                            Your order did not go through.
+                        </p>
+                        <div className="submit_btn_container" style={styles.submitContainerStyle} onClick={() => this.goHome()}>
+                            <span className="button_title">CLOSE</span>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    render () {
+        const food = this.getItem();
+
+        return (
+            <div>
+                <Navbar />
+                <div className="row">
+                    <div className="col-xs-12 col-md-12 col-lg-6">
+                        <div className="card" style={styles.imageContainerStyle}>
+
+                            <div style={{overflow: 'hidden'}}>
+                                <img className="card-img" src={food ? food.photo : ''} alt="picture of selected dish" style={styles.cardImageStyle}/>
+                            </div>
+                            <div className="card-img-overlay">
+                                <div className="price_btn_container"
+                                     style={{position: 'absolute', left: '35%'}}
+                                >
+                                    <h6 className="text-center subSubTitle" style={{display: 'block', marginTop: '1.25vmin', marginBottom: '.1vmin'}}>COST:</h6>
+                                    <span className="priceNum" style={{display: 'block'}}>${food ? food.price : ''}</span>
+                                </div>
+                                <div onClick={() => this.setState({ordered: !this.state.ordered})}
+                                     className="buy_btn_container burgundy-bg"
+                                     style={{position: 'absolute', left: '55%'}}
+                                >
+                                    <span className="buy_button_title">{this.state.ordered ? 'CANCEL' : 'BUY'}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-3">
+                        <div className="card" style={styles.containerStyle}>
+                            <h4 className="text-center subTitle"><img src={titleCooking} style={{marginBottom: '10px'}} /><br />DESCRIPTION</h4>
+                            <h6 className="text-center subSubTitle">COOKIN' TITLE:</h6>
+                            <h6 className="text-center nameTitle">{food ? food.item_name : ''}</h6>
+                            <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
+                            <h6 className="text-center subSubTitle">COOKIN' DESCRIPTION:</h6>
+                            <h6 className="mainText text-center">{food ? food.description : ''}</h6>
+                        </div>
+                    </div>
+                    <div className="col-xs-6 col-sm-6 col-md-6 col-lg-3">
+                        <div className="card" style={styles.containerStyle}>
+                            <h4 className="text-center subTitle"><img src={titleCooking} style={{marginBottom: '10px'}} /><br />PICKUP INFO</h4>
+                            <h6 className="text-center subSubTitle">COST:</h6>
+                            <h5 className="text-center mainText">${food ? food.price : ''}</h5>
+                            <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
+                            <h6 className="text-center subSubTitle">CHEF NAME:</h6>
+                            <h5 className="text-center mainText">{this.props.chef.firstName} {this.props.chef.lastName}</h5>
+                            <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
+                            <h6 className="text-center subSubTitle">CHEF ADDRESS:</h6>
+                            <h5 className="text-center mainText">{this.props.chef.address}</h5>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="row">
+                    {this.itemOrdered(food)}
+                    {this.submitted()}
+                </div>
             </div>
-          </div>
-
-
-          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-3">
-            <div className="card" style={styles.containerStyle}>
-              <h4 className="text-center subTitle"><img src={titleCooking} style={{marginBottom: '10px'}} /><br />DESCRIPTION</h4>
-              <h6 className="text-center subSubTitle">COOKIN' TITLE:</h6>
-              <h6 className="text-center nameTitle">{food ? food.item_name : ''}</h6>
-              <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
-              <h6 className="text-center subSubTitle">COOKIN' DESCRIPTION:</h6>
-              <h6 className="mainText text-center">{food ? food.description : ''}</h6>
-            </div>
-          </div>
-          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-3">
-            <div className="card" style={styles.containerStyle}>
-              <h4 className="text-center subTitle"><img src={titleCooking} style={{marginBottom: '10px'}} /><br />PICKUP INFO</h4>
-              <h6 className="text-center subSubTitle">COST:</h6>
-              <h5 className="text-center mainText">${food ? food.price : ''}</h5>
-              <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
-              <h6 className="text-center subSubTitle">CHEF NAME:</h6>
-              <h5 className="text-center mainText">{this.props.chef.firstName} {this.props.chef.lastName}</h5>
-              <div style={{width: '100%', height: '1px', backgroundColor: '#fff', marginTop: '10px', marginBottom: '15px'}}> </div>
-              <h6 className="text-center subSubTitle">CHEF ADDRESS:</h6>
-              <h5 className="text-center mainText">{this.props.chef.address}</h5>
-            </div>
-          </div>
-
-
-
-        </div>
-
-        <div className="row">
-            {this.itemOrdered()}
-
-          {/*<div className="col-xs-12 col-md-12 col-lg-6">*/}
-            {/*<div className="card map_back_01" style={styles.containerStyle}>*/}
-              {/*<h3>Map placeholder</h3>*/}
-              {/*<h5>(Chef location from order location will be placed.)</h5>*/}
-            {/*</div>*/}
-          {/*</div>*/}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 const styles = {
-  containerStyle: {
-      boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-      padding: '15px',
-      marginTop: '15px',
-      minHeight: '250px',
-      borderRadius: '0',
-      width: '100%',
-      border: '15px solid #99cccc',
-      backgroundColor: '#009999'
-  },
-  imageContainerStyle: {
-      boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-      padding: '0',
-      marginTop: '15px',
-      borderRadius: '0',
-      border: '15px solid #99cccc',
-      backgroundColor: '#009999',
-  },
-  buyContainerStyle: {
-      boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-      padding: '15px',
-      marginTop: '15px',
-      borderRadius: '0',
-      width: '100%',
-      minHeight: '350px',
-      border: '15px solid #ff908f',
-      backgroundColor: '#d85856'
-  },
-  cardImageStyle: {
-      borderRadius: '0',
-      objectFit: 'cover',
-      width: '100%',
-      height: 'auto'
-  },
-  searchInputStyle: {
-    // display: 'block',
-    //   position: 'relative',
-    boxShadow: 'none',
-    border: '1px solid',
-    float: 'left',
-    outline: 'none',
-    borderRadius: '0',
-    color: '#ffffff',
-    width:'100%',
-    marginTop: '15px',
-    padding: '35x',
-    backgroundColor: 'transparent'
-  },
-  submitContainerStyle: {
-    backgroundColor: 'transparent',
-      left: '50%',
-      marginLeft: '-3.5vmin'
-    // display: 'block'
-  }
+    containerStyle: {
+        boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+        padding: '15px',
+        marginTop: '15px',
+        minHeight: '250px',
+        borderRadius: '0',
+        width: '100%',
+        border: '15px solid #99cccc',
+        backgroundColor: '#009999'
+    },
+    imageContainerStyle: {
+        boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+        padding: '0',
+        marginTop: '15px',
+        borderRadius: '0',
+        border: '15px solid #99cccc',
+        backgroundColor: '#009999',
+    },
+    buyContainerStyle: {
+        boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+        padding: '15px',
+        marginTop: '15px',
+        borderRadius: '0',
+        width: '100%',
+        minHeight: '350px',
+        border: '15px solid #ff908f',
+        backgroundColor: '#d85856'
+    },
+    cardImageStyle: {
+        borderRadius: '0',
+        objectFit: 'cover',
+        width: '100%',
+        height: 'auto'
+    },
+    searchInputStyle: {
+        boxShadow: 'none',
+        border: '1px solid',
+        float: 'left',
+        outline: 'none',
+        borderRadius: '0',
+        color: '#ffffff',
+        width:'100%',
+        marginTop: '15px',
+        padding: '35x',
+        backgroundColor: 'transparent'
+    },
+    submitContainerStyle: {
+        // backgroundColor: 'transparent',
+        left: '50%',
+        marginLeft: '-3.5vmin'
+    }
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const {chef, id} = ownProps.match.params;
+    const {chef, id} = ownProps.match.params;
 
-  return {
-    chefId: chef,
-    menuId: id,
-    chef: state.chef,
-    menu: state.menu
-  };
+    return {
+        chefId: chef,
+        menuId: id,
+        chef: state.chef,
+        menu: state.menu,
+        checkout: state.checkout
+    };
 };
 
-export default connect(mapStateToProps, {displaySelectedChef, getMenu})(Checkout);
+export default connect(mapStateToProps, {displaySelectedChef, getMenu, nameInput, emailInput, submit})(Checkout);
